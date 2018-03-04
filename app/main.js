1f11940597276616dfb1567696bb5454ac5fe155
Michael Minshew <michael.minshew@gmail.com>
2018-03-04 08:21:08 -0600

    fixed app size that i screwed up with the last commit on.

--- app/main.js
+++ app/main.js
@@ -9,7 +9,7 @@ const {app, BrowserWindow} = require('electron')
 
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 60000, height: 450})
+    win = new BrowserWindow({width: 600, height: 450})
 
     // and load the index.html of the app.
     win.loadURL(url.format({
